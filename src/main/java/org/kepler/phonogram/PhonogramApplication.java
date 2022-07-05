e284bc2f24d98ae81f770d5d3d76f2afb84b7d63
kepler100500 <kepler007@mail.ru>
2022-07-05 11:06:34 +0400

    Init commit. Create Spring mvc carcase.

---
+++ src/main/java/org/kepler/phonogram/PhonogramApplication.java
@@ -0,0 +1,13 @@
+package org.kepler.phonogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonogramApplication.class, args);
+	}
+
+}
